157af5e151868a0ab83df2bc937ed1d7b3995271
Ianne Carl <bulilaniannecarl@gmail.com>
2026-01-08 22:16:39 +0800

    convert package to oop

--- src/routes/packageRoutes.ts
+++ src/routes/packageRoutes.ts
@@ -1,27 +1,22 @@
 import express from "express";
-import {
-  createPackage,
-  getPackageByReference,
-  updatePackage,
-  deletePackage,
-  getFilteredPackages
-} from "../controllers/packageController.ts";
+import { PackageController } from "../controllers/packageController.ts";
 
 const router = express.Router();
+const packageController = new PackageController();
 
 // Create a new package
-router.post("/create", createPackage);
+router.post("/create", packageController.createPackage);
 
 // get all + filter
-router.post("/filter", getFilteredPackages);
+router.post("/filter", packageController.getFilteredPackages);
 
 // Get package by reference
-router.get("/:reference", getPackageByReference);
+router.get("/:reference", packageController.getPackageByReference);
 
 // Update package by reference
-router.put("/:reference", updatePackage);
+router.put("/:reference", packageController.updatePackage);
 
 // Delete package by reference
-router.delete("/:reference", deletePackage);
+router.delete("/:reference", packageController.deletePackage);
 
 export default router;
