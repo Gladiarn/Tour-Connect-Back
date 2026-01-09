1a2a7f5b4179d48d1c5d78e1058e6dba8c5ea330
Ianne Carl <bulilaniannecarl@gmail.com>
2026-01-09 12:00:00 +0800

    fix hotel route

--- src/routes/hotelRoutes.ts
+++ src/routes/hotelRoutes.ts
@@ -9,7 +9,7 @@ const hotelController = new HotelController();
 // Routes using instance methods
 router.get('/all', hotelController.getAll);
 router.get('/location', hotelController.getByLocation);
-router.get('/:hotelReference/rooms/:roomReference', hotelController.getRoom);
+router.get('/:hotelReference/:roomReference', hotelController.getRoom);
 router.post('/create', hotelController.createHotel);
 router.put('/:reference', hotelController.updateHotel);
 router.delete('/:reference', hotelController.deleteHotel);
